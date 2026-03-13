7b1020b33c2239633ee135c68724f4371c29447c
Claudio Scheer <claudioscheer@protonmail.com>
2026-03-13 15:55:07 -0300

    chore: styling improvements

--- themes/meghna/assets/js/script.js
+++ themes/meghna/assets/js/script.js
@@ -14,46 +14,55 @@ jQuery(function ($) {
 	/*	Auto close Navbar when click on link
 	/* ========================================================================= */
 
-	$('.navbar-collapse a').click(function () {
-		$(".navbar-collapse").collapse('hide');
-	});
+		$('.navbar-collapse a').click(function () {
+			$(".navbar-collapse").collapse('hide');
+		});
+
+		$('.navbar-collapse').on('shown.bs.collapse hidden.bs.collapse', function () {
+			var isExpanded = $(this).hasClass('show');
+			$('.navbar-toggler').attr('aria-expanded', String(isExpanded));
+		});
 
 
 	/* ========================================================================= */
 	/*	lazy load initialize
 	/* ========================================================================= */
 
-	const observer = lozad(); // lazy loads elements with default selector as ".lozad"
-	observer.observe();
+		if (window.lozad) {
+			const observer = lozad(); // lazy loads elements with default selector as ".lozad"
+			observer.observe();
+		}
 
 	/* ========================================================================= */
 	/*	Magnific popup
 	/* =========================================================================  */
-	$('.image-popup').magnificPopup({
-		type: 'image',
-		removalDelay: 160, //delay removal by X to allow out-animation
-		callbacks: {
-			beforeOpen: function () {
-				// just a hack that adds mfp-anim class to markup
-				this.st.image.markup = this.st.image.markup.replace('mfp-figure', 'mfp-figure mfp-with-anim');
-				this.st.mainClass = this.st.el.attr('data-effect');
-			}
-		},
-		closeOnContentClick: true,
-		midClick: true,
-		fixedContentPos: false,
-		fixedBgPos: true
-	});
+		if ($.fn.magnificPopup) {
+			$('.image-popup').magnificPopup({
+				type: 'image',
+				removalDelay: 160, //delay removal by X to allow out-animation
+				callbacks: {
+					beforeOpen: function () {
+						// just a hack that adds mfp-anim class to markup
+						this.st.image.markup = this.st.image.markup.replace('mfp-figure', 'mfp-figure mfp-with-anim');
+						this.st.mainClass = this.st.el.attr('data-effect');
+					}
+				},
+				closeOnContentClick: true,
+				midClick: true,
+				fixedContentPos: false,
+				fixedBgPos: true
+			});
+		}
 
 	/* ========================================================================= */
 	/*	Portfolio Filtering Hook
 	/* =========================================================================  */
 
-	var containerEl = document.querySelector('.shuffle-wrapper');
-	if (containerEl) {
-		var Shuffle = window.Shuffle;
-		var myShuffle = new Shuffle(document.querySelector('.shuffle-wrapper'), {
-			itemSelector: '.shuffle-item',
+		var containerEl = document.querySelector('.shuffle-wrapper');
+		if (containerEl && window.Shuffle) {
+			var Shuffle = window.Shuffle;
+			var myShuffle = new Shuffle(document.querySelector('.shuffle-wrapper'), {
+				itemSelector: '.shuffle-item',
 			buffer: 1
 		});
 
@@ -69,12 +78,14 @@ jQuery(function ($) {
 	/*	Testimonial Carousel
 	/* =========================================================================  */
 
-	$("#testimonials").slick({
-		infinite: true,
-		arrows: false,
-		autoplay: true,
-		autoplaySpeed: 4000
-	});
+		if ($.fn.slick && $("#testimonials").length) {
+			$("#testimonials").slick({
+				infinite: true,
+				arrows: false,
+				autoplay: true,
+				autoplaySpeed: 4000
+			});
+		}
 
 	/* ========================================================================= */
 	/*	counter up
