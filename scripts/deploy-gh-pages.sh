aadc08fec3f9d228481e65e49f5341d58727ba26
Claudio Scheer <claudioscheer@protonmail.com>
2026-03-14 00:16:38 -0300

    Autonomous deploy Sat Mar 14 12:16:38 AM -03 2026

--- scripts/deploy-gh-pages.sh
+++ scripts/deploy-gh-pages.sh
@@ -1,5 +1,20 @@
-hugo -D
+#!/bin/bash
+set -e
+
+# Commit and push source changes on master
 git add .
-git commit -S -m "Autonomous deploy `date`"
+git commit -S -m "Autonomous deploy $(date)"
 git push
-git subtree push --prefix public origin gh-pages
+
+# Build the site
+hugo -D
+
+# Deploy public/ to gh-pages using a temporary orphan approach
+cd public
+git init
+git checkout -b gh-pages
+git add .
+git commit -m "Deploy $(date)"
+git push -f "$(cd .. && git remote get-url origin)" gh-pages
+cd ..
+rm -rf public/.git
